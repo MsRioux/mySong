348ef9ad3fcb7bff5a24168a1b5de57aa5ffb3dd
MsRioux <jrioux@mvusd.net>
2019-10-03 07:50:53 -0700

    Class construction code along - the basics

--- src/Song.java
+++ src/Song.java
@@ -3,7 +3,7 @@ public class Song
     //1) Instance Variables
     String title;
     String artist; // can also be composer
-    Boolean hasLyrics;
+    boolean hasLyrics;
     int length; //in seconds
     double fileSize; //in megaBytes
 
@@ -17,7 +17,17 @@ public class Song
         fileSize = 0.0;
     }// end zero-arg or default constructor
 
-    public Song(String newTitle, String newArtist, Boolean newHasLyrics, int newLength, double newFileSize)
+    public Song(String newTitle, String newArtist, boolean newHasLyrics)
+    {
+        title = newTitle;
+        artist = newArtist;
+        hasLyrics = newHasLyrics;
+        length = 0;
+        fileSize = 0.0;
+    }//end three-arg constructor
+
+
+    public Song(String newTitle, String newArtist, boolean newHasLyrics, int newLength, double newFileSize)
     {
         title = newTitle;
         artist = newArtist;
@@ -26,6 +36,48 @@ public class Song
         fileSize = newFileSize;
 
     }//end multi-arg constructor
+
+
+    //4 Getters
+    public String getTitle()
+    {
+        return title;
+    }//title getter
+
+    public String getArtist()
+    {
+        return artist;
+    }//artist getter
+
+    public boolean getHasLyrics()
+    {
+        return hasLyrics;
+    }//hasLyrics getter
+
+    //5 Setters
+    public void setTitle(String newTitle)
+    {
+        title = newTitle;
+    }//end Title setter
+
+    public void setArtist(String newArtist)
+    {
+        artist = newArtist;
+    }// end artist setter
+
+    public void setHasLyrics(boolean newHasLyrics)
+    {
+        hasLyrics = newHasLyrics;
+    }// end hasLyrics setter
+
+
+    //6 Brain Methods
+    public String toMinSec()
+    {
+        int minutes = length / 60;
+        int seconds = length % 60;
+       return minutes + " min " + seconds + " sec";
+    }//end brain method to convert length
 
     //3. toString  a must have for every great object class
     public String toString()
--- src/SongDriver.java
+++ src/SongDriver.java
@@ -3,14 +3,31 @@ public class SongDriver
     public static void main (String args[])
     {
         //1. Create a Song, or more
+        Song someSong = new Song();
         Song fireworks = new Song("Fireworks", "Katy Perry", true, 234, 6.94);
         Song deadLochs = new Song("Deadlochs", "Blue Face", true, 150, 3.57);
-        Song overture = new Song("1812 Overture", "Petyr Ilych Tchaicovsky", false, 1080, 11.11);
+        Song overture = new Song("1812 Overture", "Petyr Ilych Tchaicovsky", false, 1093, 11.11);
+        Song happySong = new Song("The Happy Song","Moi", true);
+
+        //3 Testing getters
+        System.out.println(fireworks.getTitle());
+        System.out.println(deadLochs.getArtist());
+        System.out.println(overture.getHasLyrics());
+
+        System.out.println("In minutes: " + overture.toMinSec());
+
+        //Testing Setters
+//        System.out.println("\n" + someSong + "\n");
+//        someSong.setTitle("Lalalalala");
+//        someSong.setArtist("A Singer");
+//        System.out.println(someSong);
+
         //2. Print it out
-        System.out.println(fireworks);
-        System.out.println();
-        System.out.println(deadLochs);
-        System.out.println("\n" + overture);
+        System.out.println(happySong);
+//        System.out.println(fireworks);
+//        System.out.println();
+//        System.out.println(deadLochs);
+//        System.out.println("\n" + overture);
     }//end main method
 
 }//end class SongDriver.  This is where all the fun stuff happens.
